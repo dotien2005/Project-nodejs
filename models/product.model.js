b4f7fc445b2b6447178a421e4935fa5fb58d572f
Do Tien <tiendt4180@ut.edu.vn>
2026-03-09 14:44:17 +0700

    1.7 models Product

---
+++ models/product.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const productschema = new mongoose.Schema({
+  title: String,
+
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  stock: Number,
+  thumbnail: String,
+  status: String,
+  position: Number,
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  deletedAt: Date,
+});
+const Product = mongoose.model("Product", productschema, "projectnodejs");
+
+module.exports = Product;
