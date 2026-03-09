05686b3ccc63ca0f6faa3b1a99f0d8d4076044d5
Do Tien <tiendt4180@ut.edu.vn>
2026-03-09 14:28:25 +0700

    1.5 connect  Mongoose_db

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 
 // 3. dùng dotenv để quản lý biến môi trường - dong PORT trong file .env
-require("dotenv").config();
 const port = process.env.PORT;
+
+//  4 static file - css, js, image
+app.use(express.static("public"));
+
+// 5 Mongoose - kết nối với mongodb
+const mongoose = require("mongoose");
+mongoose.connect(process.env.Mongo_URL);
 
 //1. dùng pug làm view engine
 app.set("views", "./views");
 app.set("view engine", "pug");
-//  4 static file - css, js, image
-app.use(express.static("public"));
 
 // 2. dùng router
 const routerClient = require("./router/client/index.router");
