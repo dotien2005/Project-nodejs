30032784894ff800b219874175fe53687cbcb33d
Do Tien <tiendt4180@ut.edu.vn>
2026-03-13 00:50:17 +0700

    2.9 khung giao diện admin/products

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,5 +1,8 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+module.exports.index = async (req, res) => {
+  const products = await Product.find();
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Products",
+    productsPug: products,
   });
 };
