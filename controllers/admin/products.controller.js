da3d7448c962c59fb4e10e57c963f2fc94032c09
Do Tien <tiendt4180@ut.edu.vn>
2026-03-13 20:33:30 +0700

    2.9 bổ sung

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+// [GET] /admin/products
 module.exports.index = async (req, res) => {
   const products = await Product.find();
   res.render("admin/pages/products/index.pug", {
