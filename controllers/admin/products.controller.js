69eeb8bcbf3328c449e94806b500944edb163cf0
Do Tien <tiendt4180@ut.edu.vn>
2026-03-13 20:59:54 +0700

    2.10 giao diện FE bộ lọc tìm kiếm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,7 +1,16 @@
 const Product = require("../../models/product.model");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find();
+  console.log(req.query.status);
+  let find = {
+    deleted: false,
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  const products = await Product.find(find);
+
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Products",
     productsPug: products,
