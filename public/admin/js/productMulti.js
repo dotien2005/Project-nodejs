0ff28ebd54a1260934a938649d13231429785462
Do Tien <tiendt4180@ut.edu.vn>
2026-03-19 22:15:20 +0700

    4.3_1 checkbox full

--- public/admin/js/productMulti.js
+++ public/admin/js/productMulti.js
@@ -1,1 +1,19 @@
 // Check box
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if (checkboxMulti) {
+  const checkAll = checkboxMulti.querySelector("input[name='checkall']");
+  const checkboxId = checkboxMulti.querySelectorAll("input[name='id']");
+  console.log(checkAll);
+  console.log(checkboxId);
+  checkAll.addEventListener("click", () => {
+    if (checkAll.checked) {
+      checkboxId.forEach((item) => {
+        item.checked = true;
+      });
+    } else {
+      checkboxId.forEach((item) => {
+        item.checked = false;
+      });
+    }
+  });
+}
