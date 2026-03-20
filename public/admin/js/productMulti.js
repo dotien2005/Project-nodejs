72df7398750e54be3a1f3950fdf534801bf1cd6d
Do Tien <tiendt4180@ut.edu.vn>
2026-03-20 20:14:47 +0700

    4.3_2 inputsId checkbox

--- public/admin/js/productMulti.js
+++ public/admin/js/productMulti.js
@@ -1,19 +1,36 @@
 // Check box
 const checkboxMulti = document.querySelector("[checkbox-multi]");
 if (checkboxMulti) {
-  const checkAll = checkboxMulti.querySelector("input[name='checkall']");
-  const checkboxId = checkboxMulti.querySelectorAll("input[name='id']");
-  console.log(checkAll);
-  console.log(checkboxId);
-  checkAll.addEventListener("click", () => {
-    if (checkAll.checked) {
-      checkboxId.forEach((item) => {
+  // tích chọn tất cả
+  const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+  const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+  console.log(inputCheckAll);
+  console.log(inputsId);
+  inputCheckAll.addEventListener("click", () => {
+    if (inputCheckAll.checked) {
+      inputsId.forEach((item) => {
         item.checked = true;
       });
     } else {
-      checkboxId.forEach((item) => {
+      inputsId.forEach((item) => {
         item.checked = false;
       });
     }
+  });
+
+  // tích chọn
+  inputsId.forEach((input) => {
+    // console.log(inputsId.length);
+    input.addEventListener("click", () => {
+      const countInputChecked = checkboxMulti.querySelectorAll(
+        "input[name='id']:checked",
+      ).length;
+      // console.log(countInputChecked);
+      if (countInputChecked == inputsId.length) {
+        inputCheckAll.checked = true;
+      } else {
+        inputCheckAll.checked = false;
+      }
+    });
   });
 }
