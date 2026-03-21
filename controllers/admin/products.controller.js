46a8264e43b7ad39cac612851ca993e8be603de5
Do Tien <tiendt4180@ut.edu.vn>
2026-03-21 20:45:01 +0700

    5.3 done xoá vĩnh viễn sản phẩm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -67,3 +67,10 @@ module.exports.changeMulti = async (req, res) => {
   }
   res.redirect(req.get("Referrer") || "/");
 };
+
+//5 DELETE /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+  const id = req.params.id;
+  await Product.deleteOne({ _id: id });
+  res.redirect(req.get("Referrer") || "/");
+};
