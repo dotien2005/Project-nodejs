6e874f4d448932ffe3c1729d9e4f08b7e51f6b1a
Do Tien <tiendt4180@ut.edu.vn>
2026-03-24 20:32:54 +0700

    5.7_2  đóng nhanh flash

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -62,8 +62,12 @@ if (buttonPagination) {
 const showAlert = document.querySelector("[show-alert]");
 if (showAlert) {
   const time = parseInt(showAlert.getAttribute("data-time"));
+  const closeAlert = showAlert.querySelector("[close-alert]");
   setTimeout(() => {
     showAlert.classList.add("alert-hidden");
   }, time);
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
 }
 //end show alert
