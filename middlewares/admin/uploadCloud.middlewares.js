4c54e46bc82c307fe16ad1cdd1998085c41a9d7a
Do Tien <tiendt4180@ut.edu.vn>
2026-08-03 21:33:22 +0700

    tách nhỏ hàm tạo uploadCloudinary

--- middlewares/admin/uploadCloud.middlewares.js
+++ middlewares/admin/uploadCloud.middlewares.js
@@ -6,33 +6,32 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
+let streamUpload = (buffer) => {
+  return new Promise((resolve, reject) => {
+    let stream = cloudinary.uploader.upload_stream((error, result) => {
+      if (result) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
 module.exports.uploadCloud = (req, res, next) => {
   if (req.file) {
-    let streamUpload = (req) => {
-      return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream((error, result) => {
-          if (result) {
-            resolve(result);
-          } else {
-            reject(error);
-          }
-        });
-
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-      });
-    };
-
-    async function upload(req) {
-      let result = await streamUpload(req);
+    const uploadToCloudinary = async (req) => {
+      let result = await streamUpload(req.file.buffer);
       // console.log(result);
 
       // console.log(result.secure_url);
       // req.body.thumbnail = result.secure_url;
       req.body[req.file.fieldname] = result.secure_url;
       next();
-    }
+    };
 
-    upload(req);
+    uploadToCloudinary(req);
   } else {
     next();
   }
