8a0300eddeabbca81d1b07c80dec46a8b55c70e7
Do Tien <tiendt4180@ut.edu.vn>
2026-08-04 22:07:13 +0700

    tách hàm tạo helper dễ quản lý

--- middlewares/admin/uploadCloud.middlewares.js
+++ middlewares/admin/uploadCloud.middlewares.js
@@ -1,32 +1,4 @@
-const cloudinary = require("cloudinary").v2;
-const streamifier = require("streamifier");
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET, // Click 'View API Keys' above to copy your API secret
-});
-
-let streamUpload = (buffer) => {
-  return new Promise((resolve, reject) => {
-    let stream = cloudinary.uploader.upload_stream((error, result) => {
-      if (result) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    });
-
-    streamifier.createReadStream(buffer).pipe(stream);
-  });
-};
-const uploadToCloudinary = async (req) => {
-  let result = await streamUpload(req.file.buffer);
-  return result.secure_url;
-  // console.log(result);
-
-  // console.log(result.secure_url);
-  // req.body.thumbnail = result.secure_url;
-};
+const uploadToCloudinary = require("../../helpers/uploadTodinary");
 module.exports.upload = async (req, res, next) => {
   if (req.file) {
     const link = await uploadToCloudinary(req);
