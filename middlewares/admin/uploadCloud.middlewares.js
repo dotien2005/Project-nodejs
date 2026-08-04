e5c76a8e348e95b02fbe0bde65b3d885475bd536
Do Tien <tiendt4180@ut.edu.vn>
2026-08-04 22:26:32 +0700

    fix xóa - bỏ qua tính năng này

--- middlewares/admin/uploadCloud.middlewares.js
+++ middlewares/admin/uploadCloud.middlewares.js
@@ -1,5 +1,34 @@
-const uploadToCloudinary = require("../../helpers/uploadTodinary");
-module.exports.upload = async (req, res, next) => {
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+let streamUpload = (buffer) => {
+  return new Promise((resolve, reject) => {
+    let stream = cloudinary.uploader.upload_stream((error, result) => {
+      if (result) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
+module.exports.uploadToCloudinary = async (req) => {
+  let result = await streamUpload(req.file.buffer);
+  return result.secure_url;
+  // console.log(result);
+
+  // console.log(result.secure_url);
+  // req.body.thumbnail = result.secure_url;
+};
+
+module.exports.uploadCloud = async (req, res, next) => {
   if (req.file) {
     const link = await uploadToCloudinary(req);
     req.body[req.file.fieldname] = link;
