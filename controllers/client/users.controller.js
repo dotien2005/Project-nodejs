aadf9f1445bf36175b287adec3fc4df2a0da3afd
Do Tien <tiendt4180@ut.edu.vn>
2026-08-07 21:57:54 +0700

    22.2 lấy các tài khoản khác trừ tài khoản đã login vào web nhờ  { $ne: value}

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,4 +1,12 @@
-module.exports.notFriend = (req, res) => {
+const User = require("../../models/user.model");
+module.exports.notFriend = async (req, res) => {
+  const userId = res.locals.user.id;
+  const users = await User.find({
+    _id: { $ne: userId },
+    status: "active",
+    deleted: false,
+  });
+  console.log(users);
   res.render("client/pages/users/not-friend", {
     title: "Not Friend",
   });
