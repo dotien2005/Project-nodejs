6a02ca5e187795576bee1d40ef25a4b67c1f9205
Do Tien <tiendt4180@ut.edu.vn>
2026-08-11 21:36:30 +0700

    add user socket for users.controller

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,5 +1,12 @@
 const User = require("../../models/user.model");
+
+const userSocket = require("../../sockets/client/users.socket");
+
 module.exports.notFriend = async (req, res) => {
+  // SOCKET
+  userSocket(res);
+  // end socket
+
   const userId = res.locals.user.id;
   const users = await User.find({
     _id: { $ne: userId },
