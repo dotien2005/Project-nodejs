f3fb5b68341f7150737d8affbcbd8374d96e67ce
Do Tien <tiendt4180@ut.edu.vn>
2026-08-12 23:07:18 +0700

    22.3 2.thêm id của a vào acceptFriend của b

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -1,10 +1,29 @@
+const User = require("../../models/user.model");
 module.exports = (res) => {
   _io.once("connection", (socket) => {
     socket.on("CLIENT_ADD_FRIEND", async (userId) => {
       const myUserId = res.locals.user.id;
       // console.log(myUserId);
-      console.log(myUserId); // id của b
-      console.log(userId); // id của a
+      // console.log(myUserId); // id của b
+      // console.log(userId); // id của a
+
+      // =======
+      // thêm id của a vào acceptFriend của b
+      const exitIdAinB = await User.findOne({
+        _id: userId,
+        acceptFriend: myUserId,
+      });
+      if (!exitIdAinB) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $push: { acceptFriend: myUserId },
+          },
+        );
+      }
+      // thêm id của b vào requestFriend của a
     });
   });
 };
