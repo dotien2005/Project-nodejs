3dac194e15ef8524c2e7d5dda4fca546b8ca9deb
Do Tien <tiendt4180@ut.edu.vn>
2026-08-12 22:30:30 +0700

    22.3 1. gửi id lên server

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -1,7 +1,10 @@
 module.exports = (res) => {
   _io.once("connection", (socket) => {
     socket.on("CLIENT_ADD_FRIEND", async (userId) => {
-      console.log(userId);
+      const myUserId = res.locals.user.id;
+      // console.log(myUserId);
+      console.log(myUserId); // id của b
+      console.log(userId); // id của a
     });
   });
 };
