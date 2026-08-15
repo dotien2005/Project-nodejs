2193cbab511b9348f5511b66eca927045b9f5e30
Do Tien <tiendt4180@ut.edu.vn>
2026-08-15 23:36:42 +0700

    22.5 create router users/request

--- router/client/users.router.js
+++ router/client/users.router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 const controler = require("../../controllers/client/users.controller");
 router.get("/not-friend", controler.notFriend);
+router.get("/request", controler.requestFriend);
 
 module.exports = router;
