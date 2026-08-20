d3713d2d4df949e639cf9d7b8f44d7e2af3f44b1
Do Tien <tiendt4180@ut.edu.vn>
2026-08-20 21:05:17 +0700

    22.5 2 socket CLIENT_CANCEL_FRIEND

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -1,6 +1,7 @@
 const User = require("../../models/user.model");
 module.exports = (res) => {
   _io.once("connection", (socket) => {
+    // 1 chức năng gửi yêu cầu kết bạn
     socket.on("CLIENT_ADD_FRIEND", async (userId) => {
       const myUserId = res.locals.user.id;
       // console.log(myUserId);
@@ -36,6 +37,47 @@ module.exports = (res) => {
           },
           {
             $push: { requestFriend: userId },
+          },
+        );
+      }
+    });
+
+    // 2 chức năng hủy yêu cầu kết bạn
+    socket.on("CLIENT_CANCEL_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+      // console.log(myUserId);
+      // console.log(myUserId); // id của b
+      // console.log(userId); // id của a
+
+      // =======
+      // xóa id của a trong acceptFriend của b
+      const exitIdAinB = await User.findOne({
+        _id: userId,
+        acceptFriend: myUserId,
+      });
+      if (exitIdAinB) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $pull: { acceptFriend: myUserId },
+          },
+        );
+      }
+      // xóa id của b vào requestFriend của a
+
+      const exitIdBinA = await User.findOne({
+        _id: myUserId,
+        requestFriend: userId,
+      });
+      if (exitIdBinA) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $pull: { requestFriend: userId },
           },
         );
       }
